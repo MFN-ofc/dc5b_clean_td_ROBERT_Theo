affd3bfe799b0672def6a272c1b09a22570af7fd
Theo Robert <contact@theo-robert.fr>
2023-02-24 16:00:48 +0100

    version python

--- exo 3 Python/Parsing_exo1rida.py
+++ exo 3 Python/Parsing_exo1rida.py
@@ -16,17 +16,13 @@ import pandas_bokeh
 import random
 
 
-#Tri colonne et header fichier final
 used_cols = [1, 2, 10]
 headercsv = ['Period', 'Data_value', 'Series_title_2']
 fichier = pd.read_csv("/Users/theorobert/Desktop/scrapping/dc5b_clean_td_ROBERT_Theo/electronic-card-transactions-december-2022-csv-tables.csv", usecols=used_cols)
 
-#Supprime ligne vide et garde Credit|Debit|Services
 fichier = fichier.dropna()
 fichier = fichier[fichier['Series_title_2'].str.contains('^(Credit|Debit|Services)$')]
 
-#reset de l'index
 fichier = fichier.reset_index(drop=True)
 fichier.index.name = 'ID'
-#fichier final
 fichier.to_csv('/Users/theorobert/Desktop/scrapping/dc5b_clean_td_ROBERT_Theo/exo 3 Python/result.csv', header=headercsv, index=True)
